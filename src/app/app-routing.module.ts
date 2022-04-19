388267377d0e712e81cdddb28e18f8ed121bd6ad
Roberto Bocanegra <rabp_91@hotmail.com>
2022-04-19 01:40:14 -0500

    creado rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompaniesAddComponent } from './companies/companies-add/companies-add.component';
+import { CompaniesEditComponent } from './companies/companies-edit/companies-edit.component';
 import { CompaniesIndexComponent } from './companies/companies-index/companies-index.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: '/companies', pathMatch: 'full' },
   { path: 'companies', component: CompaniesIndexComponent },
+  { path: 'companies/add', component: CompaniesAddComponent },
+  { path: 'companies/edit/:id', component: CompaniesEditComponent },
 ];
 
 @NgModule({
